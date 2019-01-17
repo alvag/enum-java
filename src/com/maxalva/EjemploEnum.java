ed1e272b8b904a726c74fe584030f2e33346f93b
Max Alva <alva85@gmail.com>
2019-01-16 21:57:02 -0500

    test

--- src/com/maxalva/EjemploEnum.java
+++ src/com/maxalva/EjemploEnum.java
@@ -16,7 +16,6 @@ public class EjemploEnum {
 		indicarPais(Continentes.AFRICA);
 		indicarPais(Continentes.ASIA);
 		indicarPais(Continentes.OCEANIA);
-		indicarPais(Continentes.AMERICA);
 		
 		System.out.println("");
 		
